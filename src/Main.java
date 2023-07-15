1c25365ff00fb4fa62f8bbac74592f01fbc0f219
BlackIron <violoniste2@gmail.com>
2023-07-16 03:31:47 +0700

    fix

--- src/Main.java
+++ src/Main.java
@@ -45,9 +45,9 @@ public class Main {
         var cat = 3.6;
         var paper = 763789;
 
-        dog = dog - 3;
-        cat = cat - 3;
-        paper = paper - 3;
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
 
         System.out.println(dog);
         System.out.println(cat);
